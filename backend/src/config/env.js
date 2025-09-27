d7fb2ef07a415ec950abf27df09a7f89ce90f842
fabriciogama26 <toka.fgg@gmail.com>
2025-09-26 22:47:17 -0300

    atualiçao

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 module.exports = {
   nodeEnv: process.env.NODE_ENV || 'development',
-  port: Number(process.env.PORT || 10000)
+  port: Number(process.env.PORT || 3000),
+  username: process.env.APP_USERNAME || 'admin',
+  password: process.env.APP_PASSWORD || 'admin123',
+  displayName: process.env.APP_DISPLAY_NAME || 'Administrador'
 };
-
--- backend/src/index.js
+++
@@ -1,2 +0,0 @@
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`API on :${PORT}`));
