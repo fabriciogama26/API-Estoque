b615571c88c496b340b0ecd277b7a8d848f5b0c3
fabriciogama26 <toka.fgg@gmail.com>
2025-09-27 23:41:55 -0300

    Modificaçao do css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useLocation } from 'react-router-dom'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   BarsIcon,
   DashboardIcon,
