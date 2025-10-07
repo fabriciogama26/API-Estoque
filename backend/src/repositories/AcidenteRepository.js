55ea5798a945e8f313e12596a4a66075ef2c3c9c
Fabricio Gama <83008013+fabriciogama26@users.noreply.github.com>
2025-10-06 23:09:18 -0300

    Delete backend directory

--- backend/src/repositories/AcidenteRepository.js
+++
@@ -1,9 +0,0 @@
-const BaseRepository = require('./BaseRepository');
-
-class AcidenteRepository extends BaseRepository {
-  constructor() {
-    super('Acidente');
-  }
-}
-
-module.exports = new AcidenteRepository();
--- backend/src/services/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MaterialService: require('./MaterialService'),
-  PessoaService: require('./PessoaService'),
-  EntradaService: require('./EntradaService'),
-  SaidaService: require('./SaidaService'),
-  EstoqueService: require('./EstoqueService'),
-  AuthService: require('./AuthService'),
-  AcidenteService: require('./AcidenteService'),
-}
